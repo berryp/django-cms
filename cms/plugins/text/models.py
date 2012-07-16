e0dd9e61f7eedf74a77aaba3759ffd14dec88ea9
Berry Phillips <berryphillips@gmail.com>
2012-07-16 15:32:00 -0700

    Add setting to allow disabling of HTML sanitization for the text plugin.

--- cms/plugins/text/models.py
+++ cms/plugins/text/models.py
@@ -1,7 +1,8 @@
 from cms.models import CMSPlugin
-from cms.plugins.text.utils import (plugin_admin_html_to_tags, 
+from cms.plugins.text.utils import (plugin_admin_html_to_tags,
     plugin_tags_to_admin_html, plugin_tags_to_id_list, replace_plugin_tags)
 from cms.utils.html import clean_html
+from django.conf import settings
 from django.db import models
 from django.utils.html import strip_tags
 from django.utils.text import truncate_words
@@ -12,10 +13,10 @@ _old_tree_cache = {}
 class AbstractText(CMSPlugin):
     """Abstract Text Plugin Class"""
     body = models.TextField(_("body"))
-    
+
     class Meta:
         abstract = True
-    
+
     def _set_body_admin(self, text):
         self.body = plugin_admin_html_to_tags(text)
 
@@ -30,13 +31,17 @@ class AbstractText(CMSPlugin):
                               """)
 
     search_fields = ('body',)
-    
+
     def __unicode__(self):
         return u"%s" % (truncate_words(strip_tags(self.body), 3)[:30]+"...")
-    
+
     def clean(self):
-        self.body = clean_html(self.body, full=False)
-    
+        # The CMS_TEXT_PLUGIN_CLEAN_HTML setting allows for disabling of the
+        # HTML sanitization. This is useful in situations where user's can
+        # be truested or full control is desired.
+        if getattr(settings, 'CMS_TEXTPLUGIN_CLEANHTML', True):
+            self.body = clean_html(self.body, full=False)
+
     def clean_plugins(self):
         ids = plugin_tags_to_id_list(self.body)
         plugins = CMSPlugin.objects.filter(parent=self)
@@ -55,9 +60,9 @@ class AbstractText(CMSPlugin):
 
         self.body = replace_plugin_tags(old_instance.get_plugin_instance()[0].body, replace_ids)
         self.save()
-            
+
 class Text(AbstractText):
     """
     Actual Text Class
     """
-    
+
